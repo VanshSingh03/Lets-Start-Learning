0493e065d487dcb183397ccb35f26395476edcd7
VanshSingh03 <0vanshsingh0@gmail.com>
2024-12-20 20:28:07 +0530

    Add all

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{  
+   int a,b ;
+   
+   cout << "Enter First Number : ";
+   cin >> a ;
+   cout << "Enter Second Number : ";
+   cin >> b ;
+   
+   cout << "Sum is " << (a+b) << "\n";
+   cout << "difference is " << (a-b) << "\n";
+   cout << "multiplicatin is " << (a*b) << "\n";
+   cout << "division is " << (a/b) << "\n";
+   cout << "modulo is " << (a%b) << "\n";
+   
+   return 0 ;
+}
---
+++ If- Else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    cout<<"Enter a Number : " ;
+    cin>> a ;
+
+    if (a>=0){
+        cout<<"positive";
+    }else{
+        cout<<"negative";
+    }
+    return 0 ;
+}
